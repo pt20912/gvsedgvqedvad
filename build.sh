2cd5cdc2e404cfc0a934ab8994e3fe0a2b23a0e8
pt20912 <122719561+pt20912@users.noreply.github.com>
2023-04-09 12:44:19 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ do
     if [ -f ./buildscriptallow.cc ]
     then
         echo "buildscriptallow.cc found, running command"
-        . build/envsetup.sh && lunch twrp_a52sxq-eng && mka recoveryimage
+        . build/envsetup.sh && lunch twrp_a52sxq-eng && mka recoveryimage RECOVERY_VARIANT=philz
         break
     else
         echo "buildscriptallow.cc not found, sleeping for 1 minute"
